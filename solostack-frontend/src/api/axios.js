a5f5f05a2c34d3d82b060d3008b90e77b640479e
jeaneudes1919-prog <jeaneudesatindehou@gmail.com>
2025-12-19 19:29:21 +0100

    modification vercel

--- solostack-frontend/src/api/axios.js
+++ solostack-frontend/src/api/axios.js
@@ -1,23 +1,17 @@
 import axios from 'axios';
 
-// L'URL de ton backend (Vérifie le port, c'est souvent 5000)
-const BASE_URL = 'VITE_API_URL';
-
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// --- INTERCEPTEUR (La magie) ---
-// Avant chaque requête, on regarde si on a un token dans le stockage local
-// et on l'ajoute automatiquement dans le header "Authorization"
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
